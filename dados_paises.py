4831c4a867ef76b39e1493d06f6ab14bdcf481a2
Camilavam <camilavam@al.insper.edu.br>
2024-04-24 10:27:01 -0300

    completando dados

--- dados_paises.py
+++ dados_paises.py
@@ -49,3 +49,16 @@ PAISES =  {
 
 # alfabeto para montar o nome das colunas
 ALFABETO = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+# cores para o terminal
+CORES = {
+    'reset': '\u001b[0m',
+    'red': '\u001b[31m',
+    'black': '\u001b[30m',
+    'green': '\u001b[32m',
+    'yellow': '\u001b[33m',
+    'blue': '\u001b[34m',
+    'magenta': '\u001b[35m',
+    'cyan': '\u001b[36m',
+    'white': '\u001b[37m'
+}
