b3544c67818f6f922ec06bf39043fef771f9d715
Camilavam <camilavam@al.insper.edu.br>
2024-04-23 21:25:50 -0300

    parametros iniciais

---
+++ jogo.py
@@ -0,0 +1,14 @@
+#importa função que gera números aleatórios
+from random import randint
+
+#definindo parâmetros iniciais
+xy = ''
+atkXY = ''
+comp_valido = ''
+jogador_valido = ''
+pontuacao_jogador = 5
+pontuacao_comp = 5
+jogada = 0
+posicoes_jogador = []
+posicoes_comp = []
+
