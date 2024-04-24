8bccd77f4415a9a09614063f2f07acedc373786f
Camilavam <camilavam@al.insper.edu.br>
2024-04-23 21:38:55 -0300

    definindo variáveis e tabuleiro

--- jogo.py
+++ jogo.py
@@ -1,7 +1,7 @@
 #importa função que gera números aleatórios
 from random import randint
 
-#definindo parâmetros iniciais
+#definindo variáveis iniciais
 xy = ''
 atkXY = ''
 comp_valido = ''
@@ -11,4 +11,22 @@ pontuacao_comp = 5
 jogada = 0
 posicoes_jogador = []
 posicoes_comp = []
+
+#definições do tabuleiro
+def tabuleiro(linha,coluna,):
+    tab = []
+    for i in range(linha):
+        tab.append(coluna * [0])
+    return tab
+
+def mostrar_tabjogador():
+    print("\nTabuleiro do Jogador")
+    for c in range(5):
+        print(jogador[c])
+
+def mostrar_tabcomp():
+    print("\nTabuleiro do computador")
+    for d in range(5):
+        print(ocultoComputador[d])
+    print('----------------------------------')
 
