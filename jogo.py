4a3124bf37f3e7343fc3cedef07138cd863a151b
Camilavam <camilavam@al.insper.edu.br>
2024-04-29 21:45:44 -0300

    cria mapa jogador

--- jogo.py
+++ jogo.py
@@ -15,6 +15,7 @@ colore("red","\n \n"+ titulojogo + "\n")
 time.sleep(1)
 print("\n\u001b[32mRaposa, se prepare, pois a batalha naval vai começar!!\u001b[0m\n")
 
+#mapa oponente
 mapaoponente = cria_mapa(10)
 
 paisoponente, frotaoponente  = random.choice(list(PAISES.items()))
@@ -29,9 +30,52 @@ for naviooponente, qntd_naviooponente in frotaoponente.items():
 
 alocacaooponente = aloca_navios(mapaoponente,blocosoponente)
 
+#mapa jogador
+mapajogador = cria_mapa(10)
+
+paisjogador = (input("Raposa, digite o nome do país escolhido:"))
+primeiraletra = paisjogador[0]
+restante = paisjogador[1:]
+primeiraletra = primeiraletra.upper()
+restante = restante.lower()
+paisjogador = primeiraletra + restante
+
+if paisjogador not in PAISES:
+    print("Esse país não está disponível")
+    paisjogador =(input("Raposa, digite o nome do país escolhido:"))
 
 
-print(paisoponente)
-print(frotaoponente)
-print(blocosoponente)
-print(alocacaooponente)
+frotajogador  = PAISES[paisjogador]
+print(frotajogador)
+
+linhajogador = (input("Digite a linha do mapa:"))
+lista_linha = [1,2,3,4,5,6,7,8,9,10]
+if linhajogador not in lista_linha:
+    print("Essa linha não está disponível")
+    linhajogador =(input("Digite a linha do mapa:"))
+
+
+colunajogador = (input("Digite a coluna do mapa:"))
+lista_coluna = [1,2,3,4,5,6,7,8,9,10]
+if colunajogador not in lista_coluna:
+    print("Essa coluna não está disponível")
+    colunajogador =(input("Digite a coluna do mapa:"))
+
+orientacaojogador = (input("Digite qual a orientação desejada[v,h]:"))
+lista_orientacao = ['v','h']
+if orientacaojogador not in lista_orientacao:
+    print("Essa orientacao não está disponível")
+    colunajogador =(input("Digite a orientacão do mapa:"))
+
+
+blocosjogador = []
+for naviojogador, qntd_naviojogador in frotajogador.items():
+    for tipo_navio, tamanho in CONFIGURACAO.items():
+        if naviojogador == tipo_navio:
+            num_navios = qntd_naviojogador
+            for i in range(num_navios):
+                blocosjogador.append(tamanho)
+
+alocacaojogador = posicao_suporta(mapajogador,blocosjogador,linhajogador,colunajogador,orientacaojogador)
+
+print(alocacaojogador)
